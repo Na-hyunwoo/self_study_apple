922ccae6d60b7497b04d7c03b504272bf7db49de
Na hyunwoo <dusdjeks@naver.com>
2021-10-02 11:17:12 +0900

    error detected 1

--- js/main.js
+++ js/main.js
@@ -29,13 +29,13 @@
                 messageC_translateY_in:[20,0,{start:0.5,end:0.6}],
                 messageD_translateY_in:[20,0,{start:0.7,end:0.8}],
                 messageA_opacity_out:[1, 0,{start:0.25,end:0.3}],
-                messageA_opacity_out:[1, 0,{start:0.45,end:0.5}],
-                messageA_opacity_out:[1, 0,{start:0.65,end:0.7}],
-                messageA_opacity_out:[1, 0,{start:0.85,end:0.9}],
-                messageA_translateY_out:[0,-20,{start:0.25,end:0.3}],
-                messageA_translateY_out:[0,-20,{start:0.45,end:0.5}],
-                messageA_translateY_out:[0,-20,{start:0.65,end:0.7}],
-                messageA_translateY_out:[0,-20,{start:0.85,end:0.9}],
+                messageB_opacity_out:[1, 0,{start:0.45,end:0.5}],
+                messageC_opacity_out:[1, 0,{start:0.65,end:0.7}],
+                messageD_opacity_out:[1, 0,{start:0.85,end:0.9}],
+                messageA_translateY_out:[-10,-20,{start:0.25,end:0.3}],
+                messageB_translateY_out:[-10,-20,{start:0.45,end:0.5}],
+                messageC_translateY_out:[-10,-20,{start:0.65,end:0.7}],
+                messageD_translateY_out:[-10,-20,{start:0.85,end:0.9}],
                 
             }
         },
@@ -54,10 +54,10 @@
             heightNum:5, //브라우저 높이의 5배로 scrollHeight 세팅
             scrollHeight:0,
             objs:{
-                container:document.querySelector('#scroll-section-3'),
-                messageA:document.querySelector('#scroll-section-2 ,a'),
-                messageB:document.querySelector('#scroll-section-2 ,b'),
-                messageC:document.querySelector('#scroll-section-2 ,c'),
+                container:document.querySelector('#scroll-section-2'),
+                messageA:document.querySelector('#scroll-section-2 .a'),
+                messageB:document.querySelector('#scroll-section-2 .b'),
+                messageC:document.querySelector('#scroll-section-2 .c'),
                 pinB: document.querySelector('#scroll-section-2 .b .pin'),
                 pinC: document.querySelector('#scroll-section-2 .c .pin')
             },
@@ -184,7 +184,7 @@
                 }else{
                     //out
                     objs.messageB.style.opacity=calcValues(values.messageB_opacity_out,currentYOffset);
-                    objs.messageB.style.transform=`translate3d(0,${calcValues(values.messageA_translateY_out,currentYOffset)}%,0)`;
+                    objs.messageB.style.transform=`translate3d(0,${calcValues(values.messageB_translateY_out,currentYOffset)}%,0)`;
                 }
 
                 if(scrollRatio<=0.62){
@@ -194,17 +194,17 @@
                 }else{
                     //out
                     objs.messageC.style.opacity=calcValues(values.messageC_opacity_out,currentYOffset);
-                    objs.messageC.style.transform=`translate3d(0,${calcValues(values.messageA_translateY_out,currentYOffset)}%,0)`;
+                    objs.messageC.style.transform=`translate3d(0,${calcValues(values.messageC_translateY_out,currentYOffset)}%,0)`;
                 }
 
                 if(scrollRatio<=0.82){
                     //in
                     objs.messageD.style.opacity=calcValues(values.messageD_opacity_in,currentYOffset);
-                    objs.messageD.style.transform=`translate3d(0,${calcValues(values.messageA_translateY_in,currentYOffset)}%,0)`;
+                    objs.messageD.style.transform=`translate3d(0,${calcValues(values.messageD_translateY_in,currentYOffset)}%,0)`;
                 }else{
                     //out
                     objs.messageD.style.opacity=calcValues(values.messageD_opacity_out,currentYOffset);
-                    objs.messageD.style.transform=`translate3d(0,${calcValues(values.messageA_translateY_out,currentYOffset)}%,0)`;
+                    objs.messageD.style.transform=`translate3d(0,${calcValues(values.messageD_translateY_out,currentYOffset)}%,0)`;
                 }
                 
                 
